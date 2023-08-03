344ef7b5c5bea6e740bd2fe00c489c40c642f20b
Nikolay Chereliuk <kolyanerushaev@gmail.com>
2023-08-03 14:25:53 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
@@ -15,13 +15,13 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter basename="/">
+        <HashRouter basename="/">
           <CookiesProvider>
             <ThemeProvider theme={theme}>
               <App />
             </ThemeProvider>
           </CookiesProvider>
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
